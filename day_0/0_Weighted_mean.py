f3dd9ff9b321db720f849e2189169c31098e172f
Jcq <q.roggee@gmail.com>
2019-03-10 20:48:00 -0700

    Update 0_Weighted_mean.py

--- day_0/0_Weighted_mean.py
+++ day_0/0_Weighted_mean.py
@@ -1,16 +1,18 @@
-Input Format
+# Task 
+# Given an array, X, of N integers and an array, W, representing the respective weights of 's elements, calculate and print the weighted mean of 's elements. Your answer should be rounded to a scale of  decimal place (i.e., format).
+# Input Format
 
-The first line contains an integer, , denoting the number of elements in arrays  and . 
-The second line contains  space-separated integers describing the respective elements of array . 
-The third line contains  space-separated integers describing the respective elements of array .
+# The first line contains an integer, , denoting the number of elements in arrays  and . 
+# The second line contains  space-separated integers describing the respective elements of array . 
+# The third line contains  space-separated integers describing the respective elements of array .
 
-Constraints
+# Constraints
+# n in (0, 50)
+# xi, where  is the  element of array .
+# wi, where  is the  element of array .
+# Output Format
 
-, where  is the  element of array .
-, where  is the  element of array .
-Output Format
-
-Print the weighted mean on a new line. Your answer should be rounded to a scale of  decimal place (i.e.,  format).
+# Print the weighted mean on a new line. Your answer should be rounded to a scale of  decimal place (i.e.,  format).
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 n = int(input())
